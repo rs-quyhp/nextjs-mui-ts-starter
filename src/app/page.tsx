d5ffde9f6033101a29bf05421d4c1ce20de6fcf7
quyhp <quyhp@runsystem.net>
2025-09-11 16:45:28 +0700

    97. design main content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
+import MainSlider from '@/components/main/main.slider';
+import { Container } from '@mui/material';
 import * as React from 'react';
 
 export default function HomePage() {
-  return <main></main>;
+  return (
+    <Container>
+      <MainSlider />
+      <MainSlider />
+      <MainSlider />
+    </Container>
+  );
 }
