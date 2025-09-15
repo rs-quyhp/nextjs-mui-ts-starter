568c61e7d3d2da103311e868cd6667a25b6f026d
quyhp <quyhp@runsystem.net>
2025-09-15 15:41:00 +0700

    102-103. fetch wrapper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,20 @@
 import MainSlider from '@/components/main/main.slider';
+import { sendRequest } from '@/utils/api';
 import { Container } from '@mui/material';
 import * as React from 'react';
 
-export default function HomePage() {
+export default async function HomePage() {
+  const res = await sendRequest<IBackendRes<ITrackTop[]>>({
+    url: `${process.env.NEXT_PUBLIC_API_URL}/api/v1/tracks/top`,
+    method: 'POST',
+    body: {
+      category: 'CHILL',
+      limit: 1,
+    },
+  });
+
+  console.log('Check res data ts: ', res.data);
+
   return (
     <Container>
       <MainSlider />
