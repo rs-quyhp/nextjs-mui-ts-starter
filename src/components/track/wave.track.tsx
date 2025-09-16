8be51f375dd761f7b97b65c276e218f5415dbc36
quyhp <quyhp@runsystem.net>
2025-09-16 16:02:12 +0700

    115-121. customize wavesurfer

--- src/components/track/wave.track.tsx
+++ src/components/track/wave.track.tsx
@@ -1,8 +1,10 @@
 'use client';
 
 import { useWaveSurfer } from '@/utils/customHook';
-import { Button } from '@mui/material';
+import { Button, Container, duration } from '@mui/material';
 import { useCallback, useMemo, useRef } from 'react';
+import { WaveSurferOptions } from 'wavesurfer.js';
+import './wave.scss';
 
 interface IProps {
   audio: string | null;
@@ -10,29 +12,111 @@ interface IProps {
 
 const WaveTrack = (props: IProps) => {
   const containerRef = useRef(null);
+  const timeRef = useRef(null);
+  const durationRef = useRef(null);
+  const hoverRef = useRef(null);
   const { audio } = props;
 
-  const optionMemo = useMemo(() => {
+  const optionMemo = useMemo((): Omit<WaveSurferOptions, 'container'> => {
+    let gradient, progressGradient;
+
+    if (typeof window !== 'undefined') {
+      const canvas = document.createElement('canvas');
+      const ctx = canvas.getContext('2d')!;
+
+      // Define the waveform gradient
+      gradient = ctx.createLinearGradient(0, 0, 0, canvas.height * 1.35);
+      gradient.addColorStop(0, '#656666'); // Top color
+      gradient.addColorStop((canvas.height * 0.7) / canvas.height, '#656666'); // Top color
+      gradient.addColorStop(
+        (canvas.height * 0.7 + 1) / canvas.height,
+        '#ffffff'
+      ); // White line
+      gradient.addColorStop(
+        (canvas.height * 0.7 + 2) / canvas.height,
+        '#ffffff'
+      ); // White line
+      gradient.addColorStop(
+        (canvas.height * 0.7 + 3) / canvas.height,
+        '#B1B1B1'
+      ); // Bottom color
+      gradient.addColorStop(1, '#B1B1B1'); // Bottom color
+
+      // Define the progress gradient
+      progressGradient = ctx.createLinearGradient(
+        0,
+        0,
+        0,
+        canvas.height * 1.35
+      );
+      progressGradient.addColorStop(0, '#EE772F'); // Top color
+      progressGradient.addColorStop(
+        (canvas.height * 0.7) / canvas.height,
+        '#EB4926'
+      ); // Top color
+      progressGradient.addColorStop(
+        (canvas.height * 0.7 + 1) / canvas.height,
+        '#ffffff'
+      ); // White line
+      progressGradient.addColorStop(
+        (canvas.height * 0.7 + 2) / canvas.height,
+        '#ffffff'
+      ); // White line
+      progressGradient.addColorStop(
+        (canvas.height * 0.7 + 3) / canvas.height,
+        '#F6B094'
+      ); // Bottom color
+      progressGradient.addColorStop(1, '#F6B094'); // Bottom color
+    }
+
     return {
-      waveColor: 'rgb(200, 0, 200)',
-      progressColor: 'rgb(100, 0, 100)',
       url: `/api?audio=${audio}`,
+      barWidth: 3,
+      height: 100,
+      waveColor: gradient,
+      progressColor: progressGradient,
     };
   }, []);
 
-  const { wavesurfer, isPlaying } = useWaveSurfer(containerRef, optionMemo);
+  const { wavesurfer, isPlaying } = useWaveSurfer(
+    containerRef,
+    optionMemo,
+    timeRef,
+    durationRef,
+    hoverRef
+  );
 
   const onPlayclick = useCallback(() => {
     wavesurfer?.playPause();
   }, [wavesurfer]);
 
   return (
-    <>
-      <div ref={containerRef}>
+    <Container>
+      <div ref={containerRef} className="wave-form-container">
         <h2>Wave track</h2>
+        <div ref={timeRef} className="wave-form-time">
+          0:00
+        </div>
+        <div ref={durationRef} className="wave-form-duration">
+          0:00
+        </div>
+
+        <div ref={hoverRef} className="wave-form-hover"></div>
+
+        <div
+          className="overlay"
+          style={{
+            position: 'absolute',
+            height: '30px',
+            width: '100%',
+            bottom: '0',
+            background: '#ccc',
+            backdropFilter: 'brightness(0.5)',
+          }}
+        ></div>
       </div>
       <Button onClick={onPlayclick}>{isPlaying ? 'Pause' : 'Play'}</Button>
-    </>
+    </Container>
   );
 };
 
--- src/utils/customHook.ts
+++ src/utils/customHook.ts
@@ -12,10 +12,82 @@ export const useHasMounted = () => {
 
 export const useWaveSurfer = (
   containerRef: React.RefObject<HTMLDivElement>,
-  options: Omit<WaveSurferOptions, 'container'>
+  options: Omit<WaveSurferOptions, 'container'>,
+  timeRef: React.RefObject<HTMLDivElement>,
+  durationRef: React.RefObject<HTMLDivElement>,
+  hoverRef: React.RefObject<HTMLDivElement>
 ) => {
   const [wavesurfer, setWavesuffer] = useState<WaveSurfer | null>(null);
   const [isPlaying, setIsPlaying] = useState<boolean>(false);
+
+  const onRender = (channels: any, ctx: any) => {
+    const { width, height } = ctx.canvas;
+    const barWidth = options.barWidth || 2;
+    const barGap = options.barGap || 1;
+    const barRadius = options.barRadius || 0;
+    const separationLineHeight = 0.5; // Height of the separation line
+
+    const barCount = Math.floor(width / (barWidth + barGap));
+    const step = Math.floor(channels[0].length / barCount);
+
+    const topPartHeight = height * 0.7; // Define top part height
+    const bottomPartHeight = height * 0.3; // Define bottom part height
+
+    ctx.beginPath();
+
+    for (let i = 0; i < barCount; i++) {
+      let sumTop = 0;
+      let sumBottom = 0;
+
+      for (let j = 0; j < step; j++) {
+        const index = i * step + j;
+        const topValue = Math.abs(channels[0][index] || 0);
+        const bottomValue = Math.abs(channels[1]?.[index] || 0);
+
+        sumTop += topValue;
+        sumBottom += bottomValue;
+      }
+
+      const avgTop = sumTop / step;
+      const avgBottom = sumBottom / step;
+
+      // const barHeight = (avgTop + avgBottom)/2;
+
+      const barHeight = (avgTop + avgBottom) * 1.2;
+
+      // Vertical alignment
+      let yTop = topPartHeight - barHeight * topPartHeight;
+      let yBottom = topPartHeight + barHeight * bottomPartHeight;
+
+      if (options.barAlign === 'top') {
+        yTop = 0;
+        yBottom = bottomPartHeight;
+      } else if (options.barAlign === 'bottom') {
+        yTop = height - topPartHeight;
+        yBottom = height;
+      }
+
+      ctx.rect(
+        i * (barWidth + barGap),
+        yTop,
+        barWidth,
+        barHeight * topPartHeight
+      );
+      ctx.rect(
+        i * (barWidth + barGap),
+        yBottom - barHeight * bottomPartHeight,
+        barWidth,
+        barHeight * bottomPartHeight
+      );
+    }
+
+    // // Draw separation line
+    // ctx.fillStyle = 'rgba(255, 255, 255, 0.5)';
+    // ctx.rect(0, topPartHeight - separationLineHeight / 2, width, separationLineHeight);
+
+    ctx.fill();
+    ctx.closePath();
+  };
 
   useEffect(() => {
     if (!containerRef.current) return;
@@ -23,6 +95,7 @@ export const useWaveSurfer = (
     const ws = WaveSurfer.create({
       container: containerRef.current!,
       ...options,
+      renderFunction: onRender,
     });
 
     setWavesuffer(ws);
@@ -32,13 +105,34 @@ export const useWaveSurfer = (
     };
   }, [options, containerRef]);
 
+  const formatTime = (seconds: any) => {
+    const minutes = Math.floor(seconds / 60);
+    const secondsRemainder = Math.round(seconds) % 60;
+    const paddedSeconds = `0${secondsRemainder}`.slice(-2);
+    return `${minutes}:${paddedSeconds}`;
+  };
+
   useEffect(() => {
     if (!wavesurfer) return;
 
     const subscriptions = [
       wavesurfer.on('play', () => setIsPlaying(true)),
       wavesurfer.on('pause', () => setIsPlaying(false)),
+      wavesurfer?.on(
+        'decode',
+        (duration) => (durationRef.current!.textContent = formatTime(duration))
+      ),
+      wavesurfer?.on(
+        'timeupdate',
+        (currentTime) =>
+          (timeRef.current!.textContent = formatTime(currentTime))
+      ),
     ];
+
+    containerRef.current!.addEventListener(
+      'pointermove',
+      (e) => (hoverRef.current!.style.width = `${e.offsetX}px`)
+    );
 
     return () => subscriptions.forEach((unsub) => unsub());
   }, [wavesurfer]);
