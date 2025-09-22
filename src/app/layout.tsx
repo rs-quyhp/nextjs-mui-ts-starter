c7887a9929fe474346a05eb0e4c448a83bc2024f
rs-quyhp <quyhp@runsystem.net>
2025-09-22 08:51:42 +0700

    131. client session

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
-import * as React from 'react';
-import ThemeRegistry from '@/components/theme-registry/theme.registry';
-import AppHeader from '@/components/header/app.header';
-import AppFooter from '@/components/footer/app.footer';
+import AppFooter from "@/components/footer/app.footer";
+import AppHeader from "@/components/header/app.header";
+import ThemeRegistry from "@/components/theme-registry/theme.registry";
+import * as React from "react";
+import AuthProvider from "./lib/auth.provider";
 
 export default function RootLayout({
   children,
@@ -12,9 +13,11 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <ThemeRegistry>
-          <AppHeader />
-          {children}
-          <AppFooter />
+          <AuthProvider>
+            <AppHeader />
+            {children}
+            <AppFooter />
+          </AuthProvider>
         </ThemeRegistry>
       </body>
     </html>
--- src/components/track/wave.track.tsx
+++ src/components/track/wave.track.tsx
@@ -1,17 +1,11 @@
-'use client';
+"use client";
 
-import { useWaveSurfer } from '@/utils/customHook';
-import {
-  Button,
-  Container,
-  duration,
-  IconButton,
-  Tooltip,
-} from '@mui/material';
-import { useCallback, useMemo, useRef } from 'react';
-import { WaveSurferOptions } from 'wavesurfer.js';
-import './wave.scss';
-import { Pause, PlayArrow } from '@mui/icons-material';
+import { useWaveSurfer } from "@/utils/customHook";
+import { Pause, PlayArrow } from "@mui/icons-material";
+import { Container, IconButton, Tooltip } from "@mui/material";
+import { useCallback, useMemo, useRef } from "react";
+import { WaveSurferOptions } from "wavesurfer.js";
+import "./wave.scss";
 
 interface IProps {
   audio: string | null;
@@ -24,30 +18,30 @@ const WaveTrack = (props: IProps) => {
   const hoverRef = useRef(null);
   const { audio } = props;
 
-  const optionMemo = useMemo((): Omit<WaveSurferOptions, 'container'> => {
+  const optionMemo = useMemo((): Omit<WaveSurferOptions, "container"> => {
     let gradient, progressGradient;
 
-    if (typeof window !== 'undefined') {
-      const canvas = document.createElement('canvas');
-      const ctx = canvas.getContext('2d')!;
+    if (typeof window !== "undefined") {
+      const canvas = document.createElement("canvas");
+      const ctx = canvas.getContext("2d")!;
 
       // Define the waveform gradient
       gradient = ctx.createLinearGradient(0, 0, 0, canvas.height * 1.35);
-      gradient.addColorStop(0, '#656666'); // Top color
-      gradient.addColorStop((canvas.height * 0.7) / canvas.height, '#656666'); // Top color
+      gradient.addColorStop(0, "#656666"); // Top color
+      gradient.addColorStop((canvas.height * 0.7) / canvas.height, "#656666"); // Top color
       gradient.addColorStop(
         (canvas.height * 0.7 + 1) / canvas.height,
-        '#ffffff'
+        "#ffffff"
       ); // White line
       gradient.addColorStop(
         (canvas.height * 0.7 + 2) / canvas.height,
-        '#ffffff'
+        "#ffffff"
       ); // White line
       gradient.addColorStop(
         (canvas.height * 0.7 + 3) / canvas.height,
-        '#B1B1B1'
+        "#B1B1B1"
       ); // Bottom color
-      gradient.addColorStop(1, '#B1B1B1'); // Bottom color
+      gradient.addColorStop(1, "#B1B1B1"); // Bottom color
 
       // Define the progress gradient
       progressGradient = ctx.createLinearGradient(
@@ -56,24 +50,24 @@ const WaveTrack = (props: IProps) => {
         0,
         canvas.height * 1.35
       );
-      progressGradient.addColorStop(0, '#EE772F'); // Top color
+      progressGradient.addColorStop(0, "#EE772F"); // Top color
       progressGradient.addColorStop(
         (canvas.height * 0.7) / canvas.height,
-        '#EB4926'
+        "#EB4926"
       ); // Top color
       progressGradient.addColorStop(
         (canvas.height * 0.7 + 1) / canvas.height,
-        '#ffffff'
+        "#ffffff"
       ); // White line
       progressGradient.addColorStop(
         (canvas.height * 0.7 + 2) / canvas.height,
-        '#ffffff'
+        "#ffffff"
       ); // White line
       progressGradient.addColorStop(
         (canvas.height * 0.7 + 3) / canvas.height,
-        '#F6B094'
+        "#F6B094"
       ); // Bottom color
-      progressGradient.addColorStop(1, '#F6B094'); // Bottom color
+      progressGradient.addColorStop(1, "#F6B094"); // Bottom color
     }
 
     return {
@@ -100,24 +94,24 @@ const WaveTrack = (props: IProps) => {
   const arrComments = [
     {
       id: 1,
-      avatar: 'http://localhost:8000/images/chill1.png',
+      avatar: "http://localhost:8000/images/chill1.png",
       moment: 10,
-      user: 'username 1',
-      content: 'just a comment1',
+      user: "username 1",
+      content: "just a comment1",
     },
     {
       id: 2,
-      avatar: 'http://localhost:8000/images/chill1.png',
+      avatar: "http://localhost:8000/images/chill1.png",
       moment: 30,
-      user: 'username 2',
-      content: 'just a comment3',
+      user: "username 2",
+      content: "just a comment3",
     },
     {
       id: 3,
-      avatar: 'http://localhost:8000/images/chill1.png',
+      avatar: "http://localhost:8000/images/chill1.png",
       moment: 50,
-      user: 'username 3',
-      content: 'just a comment3',
+      user: "username 3",
+      content: "just a comment3",
     },
   ];
 
@@ -131,40 +125,40 @@ const WaveTrack = (props: IProps) => {
     <Container
       sx={{
         background:
-          'linear-gradient(135deg, rgb(106, 112, 67) 0%, rgb(11, 15, 20) 100%)',
-        display: 'flex',
-        height: '400px',
-        padding: '24px',
-        marginTop: '24px',
-        gap: '24px',
-        alignItems: 'center',
+          "linear-gradient(135deg, rgb(106, 112, 67) 0%, rgb(11, 15, 20) 100%)",
+        display: "flex",
+        height: "400px",
+        padding: "24px",
+        marginTop: "24px",
+        gap: "24px",
+        alignItems: "center",
       }}
     >
       <div
         style={{
-          display: 'flex',
-          flexDirection: 'column',
-          width: '100%',
-          height: '100%',
-          justifyContent: 'space-between',
+          display: "flex",
+          flexDirection: "column",
+          width: "100%",
+          height: "100%",
+          justifyContent: "space-between",
         }}
       >
-        <div style={{ display: 'flex', flexDirection: 'column' }}>
+        <div style={{ display: "flex", flexDirection: "column" }}>
           <div
             style={{
-              display: 'flex',
-              gap: '10px',
-              justifyContent: 'flex-start',
+              display: "flex",
+              gap: "10px",
+              justifyContent: "flex-start",
             }}
           >
             <IconButton
               sx={{
-                background: '#ff6000',
-                width: '50px',
-                height: '50px',
-                color: 'white',
-                ':hover': {
-                  background: 'rgba(255, 98, 0, 0.94)',
+                background: "#ff6000",
+                width: "50px",
+                height: "50px",
+                color: "white",
+                ":hover": {
+                  background: "rgba(255, 98, 0, 0.94)",
                 },
               }}
               onClick={onPlayclick}
@@ -172,24 +166,24 @@ const WaveTrack = (props: IProps) => {
               {isPlaying ? <Pause /> : <PlayArrow />}
             </IconButton>
             <div
-              style={{ display: 'flex', flexDirection: 'column', gap: '4px' }}
+              style={{ display: "flex", flexDirection: "column", gap: "4px" }}
             >
               <h2
                 style={{
-                  margin: 'unset',
-                  padding: '4px',
-                  background: 'rgba(0, 0, 0, 0.4)',
-                  width: 'fit-content',
+                  margin: "unset",
+                  padding: "4px",
+                  background: "rgba(0, 0, 0, 0.4)",
+                  width: "fit-content",
                 }}
               >
                 quyhp's song
               </h2>
               <h3
                 style={{
-                  margin: 'unset',
-                  padding: '4px',
-                  background: 'rgba(0, 0, 0, 0.4)',
-                  width: 'fit-content',
+                  margin: "unset",
+                  padding: "4px",
+                  background: "rgba(0, 0, 0, 0.4)",
+                  width: "fit-content",
                 }}
               >
                 quyhp
@@ -210,25 +204,25 @@ const WaveTrack = (props: IProps) => {
           <div
             className="overlay"
             style={{
-              position: 'absolute',
-              height: '30px',
-              width: '100%',
-              bottom: '0',
-              backdropFilter: 'brightness(0.5)',
+              position: "absolute",
+              height: "30px",
+              width: "100%",
+              bottom: "0",
+              backdropFilter: "brightness(0.5)",
             }}
           ></div>
-          <div className="comments" style={{ position: 'relative' }}>
+          <div className="comments" style={{ position: "relative" }}>
             {arrComments.map((cmt) => (
-              <Tooltip title={cmt.content} arrow>
+              <Tooltip title={cmt.content} key={cmt.id} arrow>
                 <img
                   key={cmt.id}
                   src={cmt.avatar}
                   style={{
-                    width: '20px',
-                    height: '20px',
-                    position: 'absolute',
-                    zIndex: '3',
-                    top: '71px',
+                    width: "20px",
+                    height: "20px",
+                    position: "absolute",
+                    zIndex: "3",
+                    top: "71px",
                     left: calLeft(cmt.moment),
                   }}
                   onPointerMove={(e) => {
@@ -245,11 +239,11 @@ const WaveTrack = (props: IProps) => {
       </div>
       <div
         style={{
-          width: '250px',
-          height: '250px',
-          background: '#cccccc',
-          flexShrink: '0',
-          margin: '24px',
+          width: "250px",
+          height: "250px",
+          background: "#cccccc",
+          flexShrink: "0",
+          margin: "24px",
         }}
       ></div>
     </Container>
