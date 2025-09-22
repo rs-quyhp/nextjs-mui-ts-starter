dda71c1107924d0ac6a802550e094a9af2da748b
rs-quyhp <quyhp@runsystem.net>
2025-09-22 15:15:16 +0700

    134. credential login

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,12 +1,45 @@
 import { sendRequest } from "@/utils/api";
 import NextAuth, { AuthOptions } from "next-auth";
 import { JWT } from "next-auth/jwt";
+import CredentialsProvider from "next-auth/providers/credentials";
 import GithubProvider from "next-auth/providers/github";
 
 export const authOptions : AuthOptions = {
   secret: process.env.NO_SECRET,
   // Configure one or more authentication providers
   providers: [
+    CredentialsProvider({
+      // The name to display on the sign in form (e.g. "Sign in with...")
+      name: "Email and Password",
+      // `credentials` is used to generate a form on the sign in page.
+      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+      // e.g. domain, username, password, 2FA token, etc.
+      // You can pass any HTML attribute to the <input> tag through the object.
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "ShikamaruBH" },
+        password: { label: "Password", type: "password" }
+      },
+      async authorize(credentials, req) {
+        const res = await sendRequest<IBackendRes<JWT>>({
+          url: `${process.env.NEXT_PUBLIC_API_URL}/api/v1/auth/login`,
+          method: "POST",
+          body: {
+            password: credentials?.password,
+            username: credentials?.username
+          },
+        });
+  
+        if (res.data) {
+          // Any object returned will be saved in `user` property of the JWT
+          return res.data as any;
+        } else {
+          // If you return null then an error will be displayed advising the user to check their details.
+          return null
+  
+          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+        }
+      }
+    }),
     GithubProvider({
       clientId: process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!,
@@ -14,12 +47,12 @@ export const authOptions : AuthOptions = {
   ],
   callbacks: {
     async jwt({ token, user, account, profile, trigger }) {
-      if (trigger === 'signIn' && account?.provider === 'github') {
+      if (trigger === 'signIn' && account?.provider !== 'credentials') {
         const res = await sendRequest<IBackendRes<JWT>>({
           url: `${process.env.NEXT_PUBLIC_API_URL}/api/v1/auth/social-media`,
           method: "POST",
           body: {
-            type: "GITHUB",
+            type: account?.provider.toLocaleUpperCase(),
             username: user?.email,
           },
         });
@@ -27,6 +60,16 @@ export const authOptions : AuthOptions = {
         if (res.data)
           token = res.data
       }
+      
+      if (trigger === 'signIn'  && account?.provider === 'credentials') {
+        //@ts-ignore
+        token.access_token = user?.access_token
+        //@ts-ignore
+        token.refresh_token = user?.refresh_token
+        //@ts-ignore
+        token.user = user?.user
+      }
+      
       return token
     },
     async session({session, user, token}) {
